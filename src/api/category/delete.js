75e5030190328afd3efeafe590af3939013bc5df
or-abdillh <hans.abdillh05@gmail.com>
2021-10-30 12:36:09 +0800

    Update delete category api, update getCategory api

--- src/api/category/delete.js
+++ src/api/category/delete.js
@@ -3,21 +3,28 @@ import axios from 'axios'
 import BASE_URL from '../BASE_URL' 
 
 const deleteCategory = (category, key, callback, getCategorys) => {
-   //Create body
-   const body = { 
-      TOKEN: localStorage.getItem('TOKEN'),
-      id_category: key,
-      category: category
+   //Create data options
+   const data = { id_category: key }
+   
+   const body = {
+      category,
+      id_category: key
    }
+   
+   //Create Headers
+   const headers = { token: localStorage.getItem('TOKEN') } 
+   
    //Fetch
-   axios.post(`${BASE_URL}/deleteCategory`, body)
+   axios.delete(`${BASE_URL}/deleteCategory`, {
+      headers, data
+   })
       .then(res => {
          //Success
          if (res.data.status === 200) {
             //Set all products to uncategory
             axios.post(`${BASE_URL}/setUncategorys`, body)
                .then(res => {
-                  if (res.data.status === 200) callback(getCategorys)
+                  callback(getCategorys)
                })
                .catch(err => console.log(err))
          }
